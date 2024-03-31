9e9631cf56930ff4df51ffe165e2d0727eee07c5
Shekah <shekhahathlol@gmail.com>
2024-03-31 23:49:15 +0300

    Added UML link + accounts app

---
+++ TuwaiqConnect/accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render,redirect
+from django.http import HttpRequest,HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login,logout
+
+# Create your views here.
+
